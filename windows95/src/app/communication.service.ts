49c2e344e5ad847516d25a627e670731e1154c30
Larissa Cristina Benedito <mewmewdevart@gmail.com>
2024-01-07 18:19:45 -0300

    Adjustments in responsivity and padding: scripts(content)

--- windows95/src/app/communication.service.ts
+++ windows95/src/app/communication.service.ts
@@ -1,64 +1,109 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
+
 export class CommunicationService {
+  // Method to open a window based on index
+  openWindow(index: number): void {
+	const icon = this.iconsDesktop[index];
+	icon.isWindowOpen = 'yes';
+  }
 
-	openWindow(index: number): void {
-		const icon = this.iconsDesktop[index];
-		icon.isWindowOpen = 'yes'; 
-	}
+  // Method to minimize a window
+  minimizeWindow(window: any): void {
+	window.isWindowOpen = 'maybe';
+  }
 
-	minimizeWindow(window: any): void {
-		window.isWindowOpen = 'maybe';
-	}
-	
-	closeWindow(window: any): void {
-        window.isWindowOpen = 'no';
-    }
+  // Method to close a window
+  closeWindow(window: any): void {
+	window.isWindowOpen = 'no';
+  }
 
-	openAgainWindow(window: any): void {
-		window.isWindowOpen = 'yes';
-		//if (window.calcBarsOpened > this.displayLimit)
-		//window.isWindowOpen = false;
-	}
+  // Method to open a window again
+  openAgainWindow(window: any): void {
+	window.isWindowOpen = 'yes';
+	// Commented out condition, as it's currently commented and unclear
+	// if (window.calcBarsOpened > this.displayLimit)
+	//   window.isWindowOpen = false;
+  }
 
-	// Desktop Icons
-	iconsDesktop: any[] = [
-		{
-			name: 'Internet',
-			path: '../../assets/icons_new/icon_internet.png',
-			isWindowOpen: 'yes', // yes, no or maybe
-		},
-		{
-			name: 'My Computer',
-			path: '../../assets/icons_new/icon_computer.png',
-			isWindowOpen: 'no',
-		},
-		{
-			name: 'My Documents',
-			path: '../../assets/icons_new/icon_folder.png',
-			isWindowOpen: 'no',
-		},
-	];
+  // Desktop Icons
+  iconsDesktop: any[] = [
+	{
+		name: 'Internet',
+		path: '../../assets/icons_new/icon_internet.png',
+		isWindowOpen: 'no', // Possible values: 'yes', 'no', or 'maybe'
+	},
+	{
+		name: 'My Computer',
+		path: '../../assets/icons_new/icon_computer.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'My Documents',
+		path: '../../assets/icons_new/icon_folder.png',
+		isWindowOpen: 'no',
+	},
+  ];
 
-	//Menu Icons
-	programsOfMenu: any[] = [
-		{ name: 'Programs', icon: '../../assets/icons_new/icon_programs.png' },
-		{ name: 'Documents', icon: '../../assets/icons_new/icon_documents.png' },
-		{ name: 'Settings', icon: '../../assets/icons_new/icon_settings.png' },
-		{ name: 'Find', icon: '../../assets/icons_new/icon_find.png' },
-		{ name: 'Help', icon: '../../assets/icons_new/icon_programs.png' },
-		{ name: 'Run', icon: '../../assets/icons_new/icons_help.png' },
-	];
+  // Menu Icons
+  programsOfMenu: any[] = [
+	{
+		name: 'Programs',
+		icon: '../../assets/icons_new/icon_programs.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Documents',
+		icon: '../../assets/icons_new/icon_documents.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Settings',
+		icon: '../../assets/icons_new/icon_settings.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Find',
+		icon: '../../assets/icons_new/icon_find.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Help',
+		icon: '../../assets/icons_new/icon_programs.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Run',
+		icon: '../../assets/icons_new/icons_help.png',
+		isWindowOpen: 'no',
+	},
+  ];
 
-	toolsPrograms: any [] = [
-		{ name: 'Calculator', icon: '../../assets/icons_new/icon_calculator.png' },
-		{ name: 'Notepad', icon: '../../assets/icons_new/icon_notepad.png' },
-		{ name: 'Paint', icon: '../../assets/icons_new/icon_paint.png' },
-		{ name: 'Internet Explorer', icon: '../../assets/icons_new/icon_internet.png' },
-	]
-
-	constructor() { }
+  // Tools Programs
+  toolsPrograms: any[] = [
+	{
+		name: 'Calculator',
+		icon: '../../assets/icons_new/icon_calculator.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Notepad',
+		icon: '../../assets/icons_new/icon_notepad.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Paint',
+		icon: '../../assets/icons_new/icon_paint.png',
+		isWindowOpen: 'no',
+	},
+	{
+		name: 'Internet Explorer',
+		icon: '../../assets/icons_new/icon_internet.png',
+		isWindowOpen: 'no',
+	},
+  ];
+  constructor() { }
 }
--- windows95/src/app/icon/icon.component.ts
+++ windows95/src/app/icon/icon.component.ts
@@ -3,33 +3,37 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommunicationService } from '../communication.service';
 
 @Component({
-  selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.sass']
+	selector: 'app-icon',
+	templateUrl: './icon.component.html',
+	styleUrls: ['./icon.component.sass']
 })
 export class IconComponent implements OnInit {
-  private clickCount: { [index: number]: number } = {};
+	private clickCount: { [index: number]: number } = {};
 
-  constructor(public communicationService: CommunicationService, private cdr: ChangeDetectorRef) { }
+	constructor(public communicationService: CommunicationService, private cdr: ChangeDetectorRef) { }
 
-  onDrop(event: CdkDragDrop<any[]>): void {
-    moveItemInArray(this.communicationService.iconsDesktop, event.previousIndex, event.currentIndex);
-  }
+	// Handles the drop event when an icon is dragged and dropped
+	onDrop(event: CdkDragDrop<any[]>): void {
+		moveItemInArray(this.communicationService.iconsDesktop, event.previousIndex, event.currentIndex);
+	}
 
-  ngOnInit(): void {
-    const iconsDesktop = this.communicationService.iconsDesktop;
-  }
+	ngOnInit(): void {
+		// Initialize the iconsDesktop array from the communicationService
+		const iconsDesktop = this.communicationService.iconsDesktop;
+	}
 
-  handleIconClick(index: number): void {
-    if (!this.clickCount[index]) {
-		// First click
-      this.clickCount[index] = 1;
-    } else {
-      // Second click
-      const icon = this.communicationService.iconsDesktop[index];
-      icon.isWindowOpen = 'yes';
-      this.clickCount[index] = 0;
-      this.cdr.detectChanges();
-    }
-  }
+	// Handles the click event on an icon
+	handleIconClick(index: number): void {
+		if (!this.clickCount[index]) {
+			// First click
+			this.clickCount[index] = 1;
+		} else {
+			// Second click
+			const icon = this.communicationService.iconsDesktop[index];
+			icon.isWindowOpen = 'yes';
+			this.clickCount[index] = 0;
+			// Manually trigger change detection to update the view
+			this.cdr.detectChanges();
+		}
+	}
 }
